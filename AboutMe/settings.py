038c48f09f4545233fea37062f8aa24423992e13
Adit Damodar <aditidamodar23@gmail.com>
2024-11-09 15:37:02 +0530

    Third

--- AboutMe/settings.py
+++ AboutMe/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'calc'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
 # Add this if you want to include static files for your app
-STATICFILES_DIRS = [BASE_DIR / "static"]                       
+STATICFILES_DIRS = [BASE_DIR / "static"]   
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
